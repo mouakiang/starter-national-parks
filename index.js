243f6961b624c6905ef73c27648c430d6f2216da
mouakiang <mouakkia@gmail.com>
2023-05-06 13:45:12 -0400

    shortened length of description to 250 or less

---
+++ index.js
@@ -0,0 +1,11 @@
+const descriptions = document.querySelectorAll(".description-display");
+
+for (let desc of descriptions.values()) {
+  let content = desc.innerText;
+
+  if (content.length > 250) {
+    content = content.slice(0, 250);
+    content = content + '<a href="#">...</a>';
+  }
+  desc.innerHTML = content;
+}
