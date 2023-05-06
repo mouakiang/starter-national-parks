d8ac7de3081128497e0004f3c0a5cd95ea0f08fd
mouakiang <mouakkia@gmail.com>
2023-05-06 13:51:13 -0400

    added styling to ratings

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const descriptions = document.querySelectorAll(".description-display");
+const ratings = document.querySelectorAll(".rating-display .value");
 
 for (let desc of descriptions.values()) {
   let content = desc.innerText;
@@ -8,4 +9,13 @@ for (let desc of descriptions.values()) {
     content = content + '<a href="#">...</a>';
   }
   desc.innerHTML = content;
+}
+
+for (let rating of ratings) {
+  let ratingValue = parseFloat(rating.innerText);
+
+  if (ratingValue > 4.7) {
+    rating.classList.add("high-rating");
+    rating.classList.remove("value");
+  }
 }
